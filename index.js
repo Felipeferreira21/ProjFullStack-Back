45778ca2df7f168946ccc37aed64d732233fd3e9
felipeferreira21 <felipeferre26@hotmail.com>
2023-09-20 11:08:26 -0300

    Conectando ao front

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 const db = require('./models');
+
+// Roteadores
+const postsRouter = require('./routes/Posts')
+
+app.use("/posts", postsRouter)
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
